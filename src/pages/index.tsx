3a6e174ac5137f0a65bdf36af33ee8659bfdf8a2
Allen <tkmzor@gmail.com>
2025-07-09 21:44:55 +0200

    configure project

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+// src/pages/index.tsx
+import React from 'react';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white text-black">
+      <h1 className="text-4xl font-bold">Welcome to Cleonique Tech Limited</h1>
+    </main>
+  );
+}
